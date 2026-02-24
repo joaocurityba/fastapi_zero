d08b1ba558754298868ad8628f5c3c6058d50cb6
joaocurityba <jcurityba@gmail.com>
2026-02-24 14:35:09 -0300

    add schema de mensagem

--- tests/test_app.py
+++ tests/test_app.py
@@ -11,3 +11,9 @@ def test_read_root():
     response = client.get('/')
     assert response.json() == {'message': 'Hello World'}
     assert response.status_code == HTTPStatus.OK
+
+
+def test_read_jota():
+    response = client.get('/jota')
+    assert response.status_code == HTTPStatus.OK
+    assert '<h1> Olá Mundo </h1>' in response.text
